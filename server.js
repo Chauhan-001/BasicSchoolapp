ed9fd1903c367adbc226d336be6ff231284bb93f
user.name : "Vansh Chauhan <user.email : "rajputvansh144@gmail.com>
2026-03-06 23:20:33 +0530

    changes for deployment

--- server.js
+++ server.js
@@ -13,8 +13,7 @@ app.get("/", (req, res) => {
   res.send("School API Running");
 });
 
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
